9b934e82524e88eefc91e67f6bc47bc6e08c9e0c
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-03-23 21:25:35 +0100

    summary

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// function isAdult(age) {
+//     // Change code below this line
+//     const passed = ;
+  
+//     // Change code above this line
+//     return passed;
+//   }
+
+const userName = "Oleksii"
+const userAge = 17
+
+console.log(`Hello, my name is ${userName} and I'm ${userAge} yo.`)
